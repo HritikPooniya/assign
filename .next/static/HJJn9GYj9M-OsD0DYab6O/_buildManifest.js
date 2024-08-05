9e17b19ea8d85046b44d7745612492bd4c7254fa
Ankitkat30 <earackcompany@gmail.com>
2024-08-05 15:48:58 +0530

    frontend

---
+++ .next/static/HJJn9GYj9M-OsD0DYab6O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/2b2aa54fee883e27.css","static/chunks/pages/index-950dbeb9d040af53.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
